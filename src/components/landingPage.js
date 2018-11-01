c79a0465ad1db378bfd099d416252ba2c679d61e
Stephen Allison <stephen.allison.technology@gmail.com>
2018-11-01 00:08:08 -0400

    first commit index my app

---
+++ src/components/landingPage.js
@@ -0,0 +1,30 @@
+// auth/Signup.js
+
+import React, { Component } from "react";
+import AuthService from "./auth/auth-service";
+import { Link } from "react-router-dom";
+import ReactDOM from "react-dom";
+
+class landingPage extends Component {
+  render() {
+    return (
+      <div class="container">
+        Stephen
+        <div class="flip">
+          <div>
+            <div>is</div>
+          </div>
+          <div>
+            <div>all</div>
+          </div>
+          <div>
+            <div>in</div>
+          </div>
+        </div>
+        Allison
+      </div>
+    );
+  }
+}
+
+export default landingPage;
